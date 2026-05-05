493caa0d104a26609ffed3acb66303be7dbedfa1
skoder09 <sahilkaregaonkar@gmail.com>
2026-05-05 22:36:52 +0200

    added plotting of energy

--- gnd_state.py
+++ gnd_state.py
@@ -1,10 +1,11 @@
-import pennylane as qml
-import numpy as np
-from numpy.linalg import eigh
+import warnings
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pennylane as qml
+from numpy.linalg import eigh
 from scipy.optimize import minimize
 
-import warnings
 warnings.filterwarnings("ignore")
 
 
@@ -14,40 +15,52 @@ def site_spin2qubit(site, spin):
     """
     return 2 * site + spin
 
-def generate_hubbard_terms(n_sites, PBC = True):
+
+def generate_hubbard_terms(n_sites, PBC=True):
     """
     Generates hermitian terms for the Hubbard model on a two-leg ladder. In all code, here we assume N=4.
     Args:
         n_sites (int): Total number of sites (must be even for a 2-leg ladder).
     """
-    
+
     # Generate h_h
     h_h = []
 
-    for (u,v) in [(0,1), (2,3), (0,2), (1,3)]: #the last 2 elements are for h_v
-        for s in [0,1]:
-            term = qml.fermi.FermiWord({(0, site_spin2qubit(u,s)) : "+", (1, site_spin2qubit(v,s)) : "-"})
-            term_conj = qml.fermi.FermiWord({(0, site_spin2qubit(v,s)) : "+", (1, site_spin2qubit(u,s)) : "-"})
+    for u, v in [(0, 1), (2, 3), (0, 2), (1, 3)]:  # the last 2 elements are for h_v
+        for s in [0, 1]:
+            term = qml.fermi.FermiWord(
+                {(0, site_spin2qubit(u, s)): "+", (1, site_spin2qubit(v, s)): "-"}
+            )
+            term_conj = qml.fermi.FermiWord(
+                {(0, site_spin2qubit(v, s)): "+", (1, site_spin2qubit(u, s)): "-"}
+            )
 
-            if (u,v)==(0,1) or (u,v) == (2,3): #PBC only in horizontal direction
-                hermitian_term = qml.fermi.FermiSentence({term: -1.0-float(PBC), term_conj: -1.0-float(PBC)})   #having PBC same as horizontal coupling with double strength (for N=4 only)
+            if (u, v) == (0, 1) or (u, v) == (2, 3):  # PBC only in horizontal direction
+                hermitian_term = qml.fermi.FermiSentence(
+                    {term: -1.0 - float(PBC), term_conj: -1.0 - float(PBC)}
+                )  # having PBC same as horizontal coupling with double strength (for N=4 only)
             else:
                 hermitian_term = qml.fermi.FermiSentence({term: -1.0, term_conj: -1.0})
             h_h.append(hermitian_term)
-    
-
 
     # Generate h_v
     h_v = []
     h_v = h_h[4:]
-    h_h=h_h[:4]
+    h_h = h_h[:4]
 
-    #Generate h_U
+    # Generate h_U
 
     U = 2
     h_U = []
     for i in range(4):
-        term = qml.fermi.FermiWord({(0, site_spin2qubit(i,0)) : "+", (1, site_spin2qubit(i,0)) : "-", (2, site_spin2qubit(i,1)) : "+", (3, site_spin2qubit(i,1)) : "-"})
+        term = qml.fermi.FermiWord(
+            {
+                (0, site_spin2qubit(i, 0)): "+",
+                (1, site_spin2qubit(i, 0)): "-",
+                (2, site_spin2qubit(i, 1)): "+",
+                (3, site_spin2qubit(i, 1)): "-",
+            }
+        )
         h_U.append(qml.fermi.FermiSentence({term: U}))
     return h_h, h_v, h_U
 
@@ -59,7 +72,7 @@ for h_h_i in h_h[1:]:
     h_h_total += h_h_i
 
 h_v_total = h_v[0]
-for h_v_i in h_v[1:]: #adds together all terms of h_v
+for h_v_i in h_v[1:]:  # adds together all terms of h_v
     h_v_total += h_v_i
 
 h_U_total = h_U[0]
@@ -67,12 +80,9 @@ for h_U_i in h_U[1:]:
     h_U_total += h_U_i
 
 
-
-
-
 ####################
 non_int_Ham = h_h_total + h_v_total
-#print("HAM: ", non_int_Ham)
+# print("HAM: ", non_int_Ham)
 
 n_orbitals = 8
 n_electrons = 4
@@ -88,20 +98,20 @@ for word, coeff in non_int_Ham.items():
 eps, V = eigh(h)
 U = V.T
 
-#occupation = qml.qchem.hf_state(n_electrons, n_orbitals)   #previous approach
+# occupation = qml.qchem.hf_state(n_electrons, n_orbitals)   #previous approach
 occupation = np.zeros(n_orbitals, dtype=int)
-occupation[np.argsort(eps)[:n_electrons]] = 1 #=[1 1 1 1 0 0 0 0]
+occupation[np.argsort(eps)[:n_electrons]] = 1  # =[1 1 1 1 0 0 0 0]
 
 
 dev = qml.device("default.qubit", wires=n_orbitals)
 qubit_H = qml.jordan_wigner(non_int_Ham)
-full_Ham = qml.jordan_wigner(h_h_total+h_v_total+h_U_total)
+full_Ham = qml.jordan_wigner(h_h_total + h_v_total + h_U_total)
 
 
 def ground_state():
     qml.BasisState(occupation, wires=range(n_orbitals))
-    qml.BasisRotation(wires=range(n_orbitals), unitary_matrix=U, check = True)
-    
+    qml.BasisRotation(wires=range(n_orbitals), unitary_matrix=U, check=True)
+
 
 # @qml.qnode(dev)
 # def ground_energy():
@@ -110,7 +120,7 @@ def ground_state():
 #     return qml.expval(qubit_H)
 
 
-#E0 = ground_energy()
+# E0 = ground_energy()
 
 
 @qml.qnode(dev)
@@ -135,60 +145,58 @@ def circuit(S, theta):
         """
 
         for i, term in enumerate(h_U):
-            
             jw_term = qml.jordan_wigner(term)
-            qml.exp(jw_term, 1j*theta[step][0]/2)
+            qml.exp(jw_term, 1j * theta[step][0] / 2)
 
-        #at this point in code, circ = e^{i*theta*h_U}
+        # at this point in code, circ = e^{i*theta*h_U}
         for i, term in enumerate(h_h):
             jw_term = qml.jordan_wigner(term)
-            qml.exp(jw_term, 1j*theta[step][1])
-        #at this point in code, circ = e^{i*theta*h_U} @ e^{i*theta*h_h}
+            qml.exp(jw_term, 1j * theta[step][1])
+        # at this point in code, circ = e^{i*theta*h_U} @ e^{i*theta*h_h}
         for i, term in enumerate(h_v):
             jw_term = qml.jordan_wigner(term)
-            qml.exp(jw_term, 1j*theta[step][2])
-        #at this point in code, circ = e^{i*theta*h_U} @ e^{i*theta*h_h} @e^{i*theta*h_v}
-        
+            qml.exp(jw_term, 1j * theta[step][2])
+        # at this point in code, circ = e^{i*theta*h_U} @ e^{i*theta*h_h} @e^{i*theta*h_v}
+
         for i, term in enumerate(h_U):
             jw_term = qml.jordan_wigner(term)
-            qml.exp(jw_term, 1j*theta[step][0]/2)
-        
-        #at this point in code, circ = e^{i*theta*h_U/2} @ e^{i*theta*h_h} @e^{i*theta*h_v} @ e^{i*theta*h_U/2}
-    
+            qml.exp(jw_term, 1j * theta[step][0] / 2)
+
+        # at this point in code, circ = e^{i*theta*h_U/2} @ e^{i*theta*h_h} @e^{i*theta*h_v} @ e^{i*theta*h_U/2}
 
     return qml.expval(full_Ham)
 
 
-#----------
+# ----------
 # INITAL PARAM. GENERATION AND CIRCUIT EVAL. FOR THOSE PARAMS.
-#----------
+# ----------
 S_tot = 3
-optim_pts = 6 #from paper
+optim_pts = 6  # from paper
 
 init_pts = np.random.normal(0, 0.1, (optim_pts, S_tot, 3))
 
 exp_energy = np.array([circuit(S_tot, theta) for theta in init_pts])
 print(exp_energy)
 
- 
-#----------
+
+# ----------
 # GREEDY NOISY SEARCH
-#----------
+# ----------
 
 n_steps = 150
-step_scale = 0.15
+step_scale = 0.1
 
-for i in range(n_steps+1):
-    if i>60:
-        step_scale /= (i//60)+1
-    
+for i in range(n_steps + 1):
+    if i > 80:
+        step_scale /= (i // 80) + 1
+
     new_pts = init_pts + np.random.normal(0, step_scale, (optim_pts, S_tot, 3))
     new_exp_energy = np.array([circuit(S_tot, theta) for theta in new_pts])
 
     all_energy = np.concatenate([exp_energy, new_exp_energy])
     all_pts = np.concatenate([init_pts, new_pts], axis=0)
     idx = np.argsort(all_energy)[:6]
-    
+
     lowest_energies = all_energy[idx]
     lowest_points = all_pts[idx]
 
@@ -196,7 +204,6 @@ for i in range(n_steps+1):
     exp_energy = lowest_energies
     init_pts = lowest_points
     step_scale = 0.1
-
 
 
 # at this point exp_energy is the lowest possible energy we could find and init_pts are the corresponding points of theta
@@ -209,28 +216,26 @@ for i in range(n_steps+1):
 
 def cost_function(theta_1d):
     theta_reshaped = theta_1d.reshape((S_tot, 3))
-    
+
     energy = circuit(S_tot, theta_reshaped)
     return float(energy)
 
 
-final_res = None 
+final_res = None
 
 for i in range(6):
     result = minimize(
         fun=cost_function,
         x0=init_pts[i].flatten(),
-        method='Powell',
-        options={'disp': True, 'maxiter': 1000}
+        method="Powell",
+        options={"disp": True, "maxiter": 1000},
     )
-    
+
     if final_res == None:
         final_res = result
     else:
         if result.fun < final_res.fun:
             final_res = result
-            
-        
 
 
 # 4. Extract the results
@@ -247,8 +252,8 @@ print(f"Best parameters: {best_params}")
 # ALTERNATE BETWEEN GREEDY SEARCH AND POWELL
 # -----------
 
-n_steps = 150
-step_scale = 0.1
+n_steps = 5
+step_scale = 0.5
 prev_best_energy = 1000000
 
 tol = 1e-9
@@ -258,7 +263,9 @@ acceptance_cutoff = 15
 step_increase_factor = 1.2
 step_decrease_factor = 0.8
 
-for _ in range(100):
+best_energy_arr = []
+
+for _ in range(10):
     init_pts = np.reshape(best_params, (S_tot, 3))
     exp_energy = circuit(S_tot, init_pts)
 
@@ -285,32 +292,39 @@ for _ in range(100):
                 current_step_scale *= step_increase_factor
             else:
                 current_step_scale *= step_decrease_factor
+            print(acceptance_count)
             acceptance_count = 0
 
     # POWELL:
     result = minimize(
-            fun=cost_function,
-            x0=init_pts.flatten(),
-            method='Powell',
-            options={'disp': True, 'maxiter': 1000}
-        )
+        fun=cost_function,
+        x0=init_pts.flatten(),
+        method="Powell",
+        options={"disp": True, "maxiter": 1000},
+    )
 
     best_energy = result.fun
     best_params = result.x
 
-    if np.abs(best_energy-prev_best_energy) < tol:
+    if np.abs(best_energy - prev_best_energy) < tol:
         break
     prev_best_energy = best_energy
 
     print("--------------")
-    print(f"Best energy found ({_}): ", best_energy, " with step size:", current_step_scale)
+    print(
+        f"Best energy found ({_}): ",
+        best_energy,
+        " with step size:",
+        current_step_scale,
+    )
     print("--------------")
-
+    best_energy_arr.append(best_energy)
 
 # Target energy: -6.26500420602625
 
 
-
-
-
-
+# Plot the best energy found at each iteration
+plt.plot(best_energy_arr)
+plt.xlabel("Iteration")
+plt.ylabel("Best Energy")
+plt.show()
